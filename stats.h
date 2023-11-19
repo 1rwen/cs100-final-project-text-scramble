14bad3105b01b079be9a3fcf654189c1c4aa44b0
1rwen <rnguy137@ucr.edu>
2023-11-19 15:12:56 -0800

    removed unnecessary library

--- stats.h
+++ stats.h
@@ -1,36 +1,37 @@
 
 #ifndef STATS_H
 #define STATS_H
+#include <map>
+#include <utility>
+#include <vector>
 
-#include <iostream>
 using namespace std;
 
 class stats {
   private:
     double wpm;
     double accuracy;
-    char max_freq
+    char max_freq;
     int attritionMax;
     int wins;
-    int losses
+    int losses;
     map<char, int> frequent_errors;
     vector<pair<int, int>> botRaceRecord; // might have to have to hardcode
     vector<pair<int, int>> UserRaceRecord;
   public: 
-    void setWPM (double _wpm) { wpm = _wpm }; // done
-    void setAccuracy (double _accuracy) { accuracy = _accuracy }; // done
+  
+    void setWPM (double _wpm) { wpm = _wpm; }; // done
+    void setAccuracy (double _accuracy) { accuracy = _accuracy; }; // done
     void addError (char _missedChar, int _frequency); // done
     void outputStats();
-    double getWPM() { return wpm }; // done
-    double getAccuracy() { return accuracy }; // done
-    void setAttritionMax(int _maxScore) { attritionMax = _maxScore }; // done
-    int getAttritionMax() { return attritionMax }; // done
+    double getWPM() { return wpm; }; // done
+    double getAccuracy() { return accuracy; }; // done
+    void setAttritionMax(int _maxScore) { attritionMax = _maxScore; }; // done
+    int getAttritionMax() { return attritionMax; }; // done
     void setRaceRecord(int _wins, int _losses); 
     map<char, int> getErrors(); // done
     pair<int, int> getRaceRecords(); // done
-
-
-
-}
+    
+};
 
 #endif
